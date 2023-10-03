0b18e7ad842b5e8824f876c007868ad7716219e6
Nouhailaquahar <98102558+Nouhailaquahar@users.noreply.github.com>
2023-10-03 15:41:40 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -3,7 +3,9 @@ var customButtonContainer = document.createElement("div");
 
 // Injectez le code HTML du bouton dans l'élément div
 customButtonContainer.innerHTML = `
-    <button class="custom-button">Mon Bouton</button>
+        <div id="custom-button-container" style="position: absolute; top: 0; left: 0; right: 0; text-align: center;">
+            <button class="custom-button">Mon Bouton</button>
+        </div>
 `;
 
 // Ajoutez l'élément div à la page d'accueil
