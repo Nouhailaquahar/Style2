2e0fcb7736d3d4f51ebc1fac92b55f5a0938499b
Nouhailaquahar <98102558+Nouhailaquahar@users.noreply.github.com>
2023-10-04 08:57:43 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,17 +1,9 @@
-/*
-// Créez un élément div pour contenir le bouton
-var customButtonContainer = document.createElement("div");
-
-// Injectez le code HTML du bouton dans l'élément div
-customButtonContainer.innerHTML = `
-        <div id="custom-button-container" style="position: absolute; background-color=pink; top: 0; left: 0; right: 0; text-align: center;height: 120px;
-    width: 70px;">
-            <button class="custom-button">Mon Bouton</button>
-        </div>
-`;
-
-// Ajoutez l'élément div à la page d'accueil
-var homePage = document.getElementById("page-home");
-homePage.appendChild(customButtonContainer);
-*/
-alert(1);
+document.addEventListener("DOMContentLoaded", function() {
+    var customButton = document.createElement("button");
+    customButton.textContent = "Mon Bouton";
+    customButton.onclick = function() {
+        alert("Bouton personnalisé cliqué !");
+    };
+    var header = document.querySelector(".header");
+    header.appendChild(customButton);
+});
