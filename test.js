8071ad58137e6a21036a28adf2ad2d418bc40c2e
Nouhailaquahar <98102558+Nouhailaquahar@users.noreply.github.com>
2023-10-05 10:40:52 +0100

    Update test.js

--- test.js
+++ test.js
@@ -7,5 +7,19 @@
     var header = document.querySelector(".header");
     header.appendChild(customButton);
 });
-*/
-console.log("c'est fichier test.js");
+
+console.log("c'est fichier test.js");*/
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com'); // Remplacez par l'URL de la page à modifier
+  await page.evaluate(() => {
+    const button = document.createElement('button');
+    button.textContent = 'Nouveau Bouton';
+    document.body.appendChild(button);
+  });
+  await browser.close();
+})();
+console.log("c'est fichier test.js")
