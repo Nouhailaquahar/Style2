ebbf294bb953c79339ef26252417fcff0821a812
Nouhailaquahar <98102558+Nouhailaquahar@users.noreply.github.com>
2023-10-06 10:09:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const button = document.createElement('button');
+/*const button = document.createElement('button');
 button.textContent = 'Nouveau Bouton';
 button.addEventListener('click', function() {
     alert('Le bouton a ete clique !');
@@ -6,3 +6,35 @@ button.addEventListener('click', function() {
 
 const sidePanel = document.getElementById('side-panel');
 sidePanel.appendChild(button);
+*/
+
+
+/*document.addEventListener("DOMContentLoaded", function() {
+    var customButton = document.createElement("button");
+    customButton.textContent = "Mon Bouton";
+    customButton.onclick = function() {
+        alert("Bouton personnalisé cliqué !");
+    };
+    var header = document.querySelector(".header");
+    header.appendChild(customButton);
+});
+*/
+
+
+console.log("Ici c'est fichier test.js");
+
+
+/*const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com'); // Remplacez par l'URL de la page à modifier
+  await page.evaluate(() => {
+    const button = document.createElement('button');
+    button.textContent = 'Nouveau Bouton';
+    document.body.appendChild(button);
+  });
+  await browser.close();
+})();
+console.log("c'est fichier test.js")*/
