845b042e23ac06bb8614600822171a42d23d77a0
Nouhailaquahar <98102558+Nouhailaquahar@users.noreply.github.com>
2023-10-06 10:19:24 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+/*document.addEventListener("DOMContentLoaded", function() {
+    var customButton = document.createElement("button");
+    customButton.textContent = "Mon Bouton";
+    customButton.onclick = function() {
+        alert("Bouton personnalisé cliqué !");
+    };
+    var header = document.querySelector(".header");
+    header.appendChild(customButton);
+});
+*/
+console.log("c'est fichier test.js");
