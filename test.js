3970c7135402a85e5029ce78ce8d7814d5ef6001
Nouhailaquahar <98102558+Nouhailaquahar@users.noreply.github.com>
2023-10-06 14:25:42 +0100

    Update test.js

--- test.js
+++ test.js
@@ -16,3 +16,16 @@ const button = document.createElement('button');
 button.textContent = 'Mon Bouton';
 document.body.appendChild(button);
 */
+
+// custom-button.js
+(function () {
+    var customButton = document.createElement('button');
+    customButton.textContent = 'Mon Bouton';
+    customButton.onclick = function () {
+        alert('Le bouton a ete clique.');
+    };
+
+    // Sélectionnez l'emplacement où vous souhaitez ajouter le bouton
+    var targetElement = document.getElementById('side-panel');
+    targetElement.appendChild(customButton);
+})();
