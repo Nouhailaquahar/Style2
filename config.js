21e9bdc3c3ed40378a240d00a84b9f223d4ecbf7
Nouhailaquahar <98102558+Nouhailaquahar@users.noreply.github.com>
2023-10-06 09:33:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Créez une requête XMLHttpRequest
-    var xhr = new XMLHttpRequest();
-
-    // Configurez la requête avec l'URL du fichier distant
-    var url = "https://raw.githubusercontent.com/Nouhailaquahar/Style2/main/custom.js";
-    xhr.open("GET", url, true);
-
-    // Configurez les en-têtes CORS pour permettre l'accès à Jenkins
-    xhr.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:8080/");
-
-    // Gérez la réponse lorsque la requête est terminée
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            // Traitez la réponse ici
-            var response = xhr.responseText;
-            console.log(response);
-        }
-    };
-
-    // Envoyez la requête
-    xhr.send();
-});
+// Ajouter un bouton à la div avec l'ID "side-panel"
+const button = document.createElement('button');
+button.textContent = 'Nouveau Bouton';
+document.getElementById('side-panel').appendChild(button);
