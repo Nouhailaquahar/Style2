f50120fcb5da4d38b44ce5653c6054760d80e261
Nouhailaquahar <98102558+Nouhailaquahar@users.noreply.github.com>
2023-10-09 14:50:43 +0100

    Update test.js

--- test.js
+++ test.js
@@ -9,31 +9,6 @@
 });
 */
 console.log("Ce message vient du fichier test.js");
-
-
-import fetch from 'node-fetch'; // Importation ESM
-const nodeFetch = require('node-fetch'); // Importation CommonJS
-
-const githubUrl = 'https://raw.githubusercontent.com/Nouhailaquahar/Style2/main/index.html';
-
-if (fetch.default) {
-  // Pour les modules ESM, utilisez le module ESM
-  fetch(githubUrl)
-    .then(response => response.text())
-    .then(htmlContent => {
-      console.log(htmlContent); // Affiche le contenu HTML dans la console
-    })
-    .catch(error => console.error(error);
-} else {
-  // Pour les modules CommonJS, utilisez le module CommonJS
-  nodeFetch(githubUrl)
-    .then(response => response.text())
-    .then(htmlContent => {
-      console.log(htmlContent); // Affiche le contenu HTML dans la console
-    })
-    .catch(error => console.error(error);
-}
-
 /*
 // example.js
 const button = document.createElement('button');
